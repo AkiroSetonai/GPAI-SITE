775948ad3b0f4110afef38ca33176cfa0ee854f6
AkiroSetonai <wallyson.cavalcante@gmail.com>
2025-04-29 16:55:31 -0300

    fix: algumas melhorias na estilização do quick-form

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { MainComponent } from './main/main.component';
 import { HeaderComponent } from '../../components/header/header.component';
 import { AboutSectionComponent } from './about-section/about-section.component';
 import { QuickFormComponent } from './quick-form/quick-form.component';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,7 @@ import { QuickFormComponent } from './quick-form/quick-form.component';
     HeaderComponent,
     AboutSectionComponent,
     QuickFormComponent,
+    CardComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
--- src/app/pages/home/quick-form/quick-form.component.ts
+++ src/app/pages/home/quick-form/quick-form.component.ts
@@ -4,8 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-quick-form',
   imports: [],
   templateUrl: './quick-form.component.html',
-  styleUrl: './quick-form.component.scss'
+  styleUrl: './quick-form.component.scss',
 })
-export class QuickFormComponent {
-
-}
+export class QuickFormComponent {}
