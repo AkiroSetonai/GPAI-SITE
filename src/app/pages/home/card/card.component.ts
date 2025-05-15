a9a7fc64512805ce60d5e7da54fa49d08f80237e
AkiroSetonai <wallyson.cavalcante@gmail.com>
2025-05-15 10:25:44 -0300

    Fix: Carrosel corrigido

--- src/app/pages/home/card/card.component.ts
+++ src/app/pages/home/card/card.component.ts
@@ -1,21 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-card',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss',
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit, OnDestroy {
   images = [
     { src: '/template-animals/gato1.png', alt: 'Gato 1' },
-    { src: '/template-animals/cachorro.png', alt: 'Cachorro ' },
+    { src: '/template-animals/cachorro.png', alt: 'Cachorro' },
     { src: '/template-animals/gato2.png', alt: 'Gato 2' },
   ];
 
   currentIndex = 0;
   intervalId: any;
+
+  constructor(private ngZone: NgZone) {}
 
   ngOnInit() {
     this.startAutoplay();
@@ -26,9 +29,13 @@ export class CardComponent implements OnInit, OnDestroy {
   }
 
   startAutoplay() {
-    this.intervalId = setInterval(() => {
-      this.nextSlide();
-    }, 3000);
+    this.ngZone.runOutsideAngular(() => {
+      this.intervalId = setInterval(() => {
+        this.ngZone.run(() => {
+          this.nextSlide();
+        });
+      }, 3000);
+    });
   }
 
   nextSlide() {
